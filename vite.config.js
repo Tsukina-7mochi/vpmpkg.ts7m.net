94686e1aa3c27fb9ea492fad4456f0bb5e38992a
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-10-25 23:47:53 +0900

    feat: add english page

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import * as path from 'node:path';
+
+const root = path.resolve(__dirname);
+const outDir = path.resolve(__dirname, 'dist');
 
 export default defineConfig({
   base: "./",
+  build: {
+    outDir,
+    rollupOptions: {
+      input: {
+        index: path.resolve(root, 'index.html'),
+        indexJp: path.resolve(root, 'index-jp.html'),
+      }
+    }
+  }
 });
